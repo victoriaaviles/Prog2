807a4ad8893bbebe55f7408145bfd04c26ae3943
Victoria Aviles <victoria.aviles.chavez@gmail.com>
2026-09-20 17:26:17 -0300

    add comentarios em argumentos.c

--- argumentos.c
+++ argumentos.c
@@ -3,83 +3,86 @@
 #include <string.h>
 #include "argumentos.h"
 
-int parse_argumentos(int argc, char **argv, Argumentos *args)
+int parse_argumentos(int argc, char **argv, Argumentos *args) //argc = quantidade total de argumentos   argv = vetor que guarda essas strings
 {
-    args->diretorio = NULL;
-    args->saida = NULL;
-    args->vizinhos = 0;
-    args->diretorio_imagens = NULL;
+  args->diretorio = NULL;
+  args->saida = NULL;
+  args->vizinhos = 0;
+  args->diretorio_imagens = NULL;
 
-    for (int i = 1; i < argc; i++)
+  for (int i = 1; i < argc; i++)     //percorre todos os argumentos passados na linha     argv[0] guarda o executavel
+  {
+    if (strcmp(argv[i], "-d") == 0)     //compara a string atual com -d
     {
-        if (strcmp(argv[i], "-d") == 0) 
-        {
-            if (i + 1 < argc) 
-            {
-                args->diretorio = argv[++i];
-            }
-            else 
-            {
-                fprintf(stderr, "Erro: A opção -d exige um argumento.\n");
-                return 0;
-            }
-        }
-        else if (strcmp(argv[i], "-o") == 0) 
-        {
-            if (i + 1 < argc) 
-            {
-                args->saida = argv[++i];
-            } 
-            else 
-            {
-                fprintf(stderr, "Erro: A opção -o exige um argumento.\n");
-                return 0;
-            }
-        } 
-        else if (strcmp(argv[i], "-n") == 0) 
-        {
-            if (i + 1 < argc) 
-            {
-                args->vizinhos = atoi(argv[++i]);
-                if (args->vizinhos != 8 && args->vizinhos != 16) 
-                {
-                    fprintf(stderr, "Erro: Número de vizinhos deve ser 8 ou 16.\n");
-                    return 0;
-                }
-            } 
-            else 
-            {
-                fprintf(stderr, "Erro: A opção -n exige um argumento.\n");
-                return 0;
-            }
-        } 
-        else if (strcmp(argv[i], "-i") == 0) 
-        {
-            if (i + 1 < argc) 
-            {
-                args->diretorio_imagens = argv[++i];
-            } 
-            else 
-            {
-                fprintf(stderr, "Erro: A opção -i exige um argumento.\n");
-                return 0;
-            }
-        } 
-        else 
-        {
-            // Tratamento obrigatório para opções desconhecidas
-            fprintf(stderr, "Erro: Opção desconhecida '%s'.\n", argv[i]);
-            return 0;
-        }
-    }
-
-    // Validação básica para garantir que os parâmetros obrigatórios foram preenchidos
-    if (!args->diretorio || !args->saida || args->vizinhos == 0) 
-    {
-        fprintf(stderr, "Uso: %s -d <diretorio> -o <saida> -n <8|16> [-i <dir_imagens>]\n", argv[0]);
+      if (i + 1 < argc)    //verifica se há um próximo argumento
+      {
+        args->diretorio = argv[++i];    
+      }
+      else 
+      {
+        fprintf(stderr, "Erro: A opção -d exige um argumento.\n");
         return 0;
+      }
     }
 
-    return 1; // Sucesso
-}
+    else if (strcmp(argv[i], "-o") == 0)    //compara a string atual com -o
+    {
+      if (i + 1 < argc) 
+      {
+        args->saida = argv[++i];
+      } 
+      else 
+      {
+        fprintf(stderr, "Erro: A opção -o exige um argumento.\n");
+        return 0;
+      }
+    } 
 
+    else if (strcmp(argv[i], "-n") == 0)      //compara a string atual com -n
+    {
+      if (i + 1 < argc) 
+      {
+        args->vizinhos = atoi(argv[++i]);
+        if (args->vizinhos != 8 && args->vizinhos != 16) 
+        {
+          fprintf(stderr, "Erro: Número de vizinhos deve ser 8 ou 16.\n");
+          return 0;
+        }
+      } 
+      else 
+      {
+        fprintf(stderr, "Erro: A opção -n exige um argumento.\n");
+        return 0;
+      }
+    } 
+
+    else if (strcmp(argv[i], "-i") == 0)     //compara a string atual com -i
+    {
+      if (i + 1 < argc) 
+      {
+        args->diretorio_imagens = argv[++i];
+      } 
+      else 
+      {
+        fprintf(stderr, "Erro: A opção -i exige um argumento.\n");
+        return 0;
+      }
+    } 
+
+    else 
+    {
+      // para opções desconhecidas
+      fprintf(stderr, "Erro: Opção desconhecida '%s'.\n", argv[i]);
+      return 0;
+    }
+  }
+  
+  // validação para garantir que os parâmetros obrigatórios foram preenchidos
+  if (!args->diretorio || !args->saida || args->vizinhos == 0) 
+  {
+    fprintf(stderr, "Uso: %s -d <diretorio> -o <saida> -n <8|16> [-i <dir_imagens>]\n", argv[0]);
+    return 0;
+  }
+
+  return 1; // sucesso
+}
--- argumentos.h
+++ argumentos.h
@@ -1,11 +1,12 @@
 #ifndef ARGUMENTOS_H
 #define ARGUMENTOS_H
 
-typedef struct {
-    char *diretorio;         // Diretório contendo as imagens PGM (-d)
-    char *saida;             // Arquivo de saída para o histograma (-o)
-    int vizinhos;            // Número de vizinhos: 8 ou 16 (-n)
-    char *diretorio_imagens; // Diretório opcional para salvar imagens LBP (-i)
+typedef struct 
+{
+  char *diretorio;         // Diretório contendo as imagens PGM (-d)
+  char *saida;             // Arquivo de saída para o histograma (-o)
+  int vizinhos;            // Número de vizinhos: 8 ou 16 (-n)
+  char *diretorio_imagens; // Diretório opcional para salvar imagens LBP (-i)
 } Argumentos;
 
 int parse_argumentos(int argc, char **argv, Argumentos *args);
